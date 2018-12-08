55b79b9611e456d42dc75216ccb4fc8e7bea68b7
RumenRT <33309rt@gmail.com>
2018-12-07 23:08:28 -0500

    commit 2

--- 123JsQs.js
+++ 123JsQs.js
@@ -19,3 +19,39 @@
 //   console.log(y);  // Output: ReferenceError: y is not defined
 // Ref Link:
 // http://stackoverflow.com/questions/20822022/javascript-variable-definition-declaration
+
+// Question 2. For which value of x the results of the following statements are not the same ?
+// //  if( x <= 100 ) {...}
+// if (!(x > 100)) {... }
+// Answer
+// NaN <= 100 is false and NaN > 100 is also false, so if the value of x is NaN, the statements are not the same.
+
+// The same holds true for any value of x that being converted to Number, returns NaN, e.g.: undefined, [1, 2, 5], { a: 22 }, etc.
+
+// This is why you need to pay attention when you deal with numeric variables.NaN can’t be equal, less than or more than any other numeric value, so the only reliable way to check if the value is NaN, is to use isNaN() function.
+
+// Question 3. What is the drawback of declaring methods directly in JavaScript objects ?
+//   Answer
+// One of the drawback of declaring methods directly in JavaScript objects is that they are very memory inefficient.When you do that, a new copy of the method is created for each instance of an object.Let's see it on example:
+
+// var Employee = function (name, company, salary) {
+//   this.name = name || "";
+//   this.company = company || "";
+//   this.salary = salary || 5000;
+
+//   // We can create a method like this:
+//   this.formatSalary = function () {
+//     return "$ " + this.salary;
+//   };
+// };
+
+// // we can also create method in Employee's prototype:
+// Employee.prototype.formatSalary2 = function () {
+//   return "$ " + this.salary;
+// }
+
+// //creating objects
+// var emp1 = new Employee('Yuri Garagin', 'Company 1', 1000000);
+// var emp2 = new Employee('Dinesh Gupta', 'Company 2', 1039999);
+// var emp3 = new Employee('Erich Fromm', 'Company 3', 1299483);
+// Here each instance variable emp1, emp2, emp3 has own copy of formatSalary method.However the formatSalary2 will only be added once to an object Employee.prototype.
