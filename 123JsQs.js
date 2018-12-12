5efaed4ae0bdfc33bc28fae6277029949e1b2878
RumenRT <33309rt@gmail.com>
2018-12-11 21:40:00 -0500

    q5

--- 123JsQs.js
+++ 123JsQs.js
@@ -93,3 +93,26 @@
 // innerArg = 5
 // innerFuncVar = y
 // globalVar = abc
+
+// Question 5. Write a mul function which will work properly when invoked with following syntax.
+//   console.log(mul(2)(3)(4)); // output : 24
+// console.log(mul(4)(3)(4)); // output : 48
+// Answer
+// Below is the code followed by the explanation of how it works:
+
+// function mul(x) {
+//   return function (y) { // anonymous function
+//     return function (z) { // anonymous function
+//       return x * y * z;
+//     };
+//   };
+// }
+// Here the mul function accepts the first argument and returns the anonymous function which takes the second parameter and returns the anonymous function which takes the third parameter and returns the multiplication of arguments which is being passed in successive
+
+// In Javascript function defined inside has access to outer function variable and function is the first class object so it can be returned by the function as well and passed as an argument in another function.
+
+// A function is an instance of the Object type
+// A function can have properties and has a link back to its constructor method
+// A function can be stored as variable
+// A function can be pass as a parameter to another function
+//   A function can be returned from another function
