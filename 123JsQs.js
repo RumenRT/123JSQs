26688695458e4fa9f7b47bc144dade4ba2d7a97b
RumenRT <33309rt@gmail.com>
2018-12-13 20:41:15 -0500

    q7

--- 123JsQs.js
+++ 123JsQs.js
@@ -117,46 +117,46 @@
 // A function can be pass as a parameter to another function
 //   A function can be returned from another function
 
-// Question 6. How to empty an array in JavaScript ?
-//   For instance:
+// Question 7. How to check if an object is an array or not?
+// Answer
+// The best way to find whether an object is instance of a particular class or not using toString method from Object.prototype
 
-// var arrayList = ['a', 'b', 'c', 'd', 'e', 'f'];
-// How can we empty the array above ?
+// var arrayList = [1, 2, 3];
+// One of the best use cases of type checking of an object is when we do method overloading in JavaScript.To understand this, let's say we have a method called greet which can take a single string and also a list of strings. To make our greet method workable in both situation we need to know what kind of parameter is being passed: is it single value or list of values?
 
-//   Answer
-// There are a couple of ways by which we can empty an array, So let's discuss all the possible way by which we can empty an array.
-
-// Method 1
-// arrayList = [];
-// The code above will set the variable arrayList to a new empty array.This is recommended if you don't have references to the original array arrayList anywhere else because It will actually create a new empty array. You should be careful with this way of empty the array, because if you have referenced this array from another variable, then the original reference array will remain unchanged, Only use this way if you have only referenced the array by its original variable arrayList.
-
-// For instance:
-
-// var arrayList = ['a', 'b', 'c', 'd', 'e', 'f']; // Created array
-// var anotherArrayList = arrayList;  // Referenced arrayList by another variable
-// arrayList = []; // Empty the array
-// console.log(anotherArrayList); // Output ['a', 'b', 'c', 'd', 'e', 'f']
-// Method 2
-// arrayList.length = 0;
-// The code above will clear the existing array by setting its length to 0. This way of emptying an array will also update all the reference variables that point to the original array.
-
-// For instance:
-
-// var arrayList = ['a', 'b', 'c', 'd', 'e', 'f']; // Created array
-// var anotherArrayList = arrayList;  // Referenced arrayList by another variable
-// arrayList.length = 0; // Empty the array by setting length to 0
-// console.log(anotherArrayList); // Output []
-// Method 3
-// arrayList.splice(0, arrayList.length);
-// Above implementation will also work perfectly.This way of empty the array will also update all the references of the original array.
-
-// var arrayList = ['a', 'b', 'c', 'd', 'e', 'f']; // Created array
-// var anotherArrayList = arrayList;  // Referenced arrayList by another variable
-// arrayList.splice(0, arrayList.length); // Empty the array by setting length to 0
-// console.log(anotherArrayList); // Output []
-// Method 4
-// while (arrayList.length) {
-//   arrayList.pop();
+// function greet(param) {
+//   if () {
+//     // here have to check whether param is array or not
+//   }
+//   else {
+//   }
 // }
-// Above implementation can also empty the array.But not recommended to use often.
+// However, in the above implementation it might not necessary to check the type of the array, we can check for single value string and put array logic code in else block, let see below code for the same.
 
+//  function greet(param) {
+//     if (typeof param === 'string') {
+//     }
+//     else {
+//       // If param is of type array then this block of code would execute
+//     }
+//   }
+// Now it's fine we can go with the previous two implementations, but when we have a situation like a parameter can be single value, array, and object type then we will be in trouble.
+
+// Coming back to checking the type of an object, As we mentioned that we can use Object.prototype.toString
+
+// if (Object.prototype.toString.call(arrayList) === '[object Array]') {
+//   console.log('Array!');
+// }
+// If you are using jQuery then you can also used jQuery isArray method:
+
+// if ($.isArray(arrayList)) {
+//   console.log('Array');
+// } else {
+//   console.log('Not an array');
+// }
+// FYI jQuery uses Object.prototype.toString.call internally to check whether an object is an array or not.
+
+// In modern browser, you can also use:
+
+// Array.isArray(arrayList);
+// Array.isArray is supported by Chrome 5, Firefox 4.0, IE 9, Opera 10.5 and Safari 5
